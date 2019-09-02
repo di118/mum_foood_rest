ba49bac423d10f2c857a95b31c9faa2afe1f6d73
Daniel Ionita <daniel@regentsitsolutions.co.uk>
2019-09-02 20:55:28 +0100

    adding routers

--- mum_food_rest/recipe/urls.py
+++ mum_food_rest/recipe/urls.py
@@ -1,12 +1,44 @@
 from django.urls import path
 from recipe import views
 from rest_framework.urlpatterns import format_suffix_patterns
+# from recipe.views import RecipeViewSet, UserViewSet, api_root
+from rest_framework import renderers
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'recipe', views.RecipeViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('recipe/', views.RecipeList.as_view()),
-    path('recipe/<int:pk>/', views.RecipeDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', include(router.urls)),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+
+# recipe_list = RecipeViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# recipe_detail = RecipeViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+#
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('recipe/', recipe_list, name='recipe-list'),
+#     path('recipe/<int:pk>/', recipe_detail, name='recipe-detail'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
--- mum_food_rest/recipe/views.py
+++ mum_food_rest/recipe/views.py
@@ -18,25 +18,28 @@ from recipe.serializers import UserSerializer
 from rest_framework import permissions
 from recipe.permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
-
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
+
+    """
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -44,104 +47,115 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
 
-class RecipeList(generics.ListCreateAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
 
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
-
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
-# @api_view(['GET', 'POST'])
-# def recipe_list(request, format=None):
-#     """
-#     List all code snippets, or create a new snippet.
-#     """
-#     if request.method == 'GET':
-#         recipe = Recipe.objects.all()
-#         serializer = RecipeSerializer(recipe, many=True)
-#         return Response(serializer.data)
+# class RecipeList(generics.ListCreateAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer
 #
-#     elif request.method == 'POST':
-#         serializer = RecipeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# class RecipeList(APIView):
-#     """
-#     List all snippets, or create a new snippet.
-#     """
-#     def get(self, request, format=None):
-#         recipe = Recipe.objects.all()
-#         serializer = RecipeSerializer(recipe, many=True)
-#         return Response(serializer.data)
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 #
-#     def post(self, request, format=None):
-#         serializer = RecipeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def recipe_detail(request, pk, format=None):
-#     """
-#     Retrieve, update or delete a code snippet.
-#     """
-#     try:
-#         recipe = Recipe.objects.get(pk=pk)
-#     except Recipe.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
 #
-#     if request.method == 'GET':
-#         serializer = RecipeSerializer(recipe)
-#         return Response(serializer.data)
+# class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer
 #
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = RecipeSerializer(recipe, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#                           IsOwnerOrReadOnly]
+# # @api_view(['GET', 'POST'])
+# # def recipe_list(request, format=None):
+# #     """
+# #     List all code snippets, or create a new snippet.
+# #     """
+# #     if request.method == 'GET':
+# #         recipe = Recipe.objects.all()
+# #         serializer = RecipeSerializer(recipe, many=True)
+# #         return Response(serializer.data)
+# #
+# #     elif request.method == 'POST':
+# #         serializer = RecipeSerializer(data=request.data)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return Response(serializer.data, status=status.HTTP_201_CREATED)
+# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# # class RecipeList(APIView):
+# #     """
+# #     List all snippets, or create a new snippet.
+# #     """
+# #     def get(self, request, format=None):
+# #         recipe = Recipe.objects.all()
+# #         serializer = RecipeSerializer(recipe, many=True)
+# #         return Response(serializer.data)
+# #
+# #     def post(self, request, format=None):
+# #         serializer = RecipeSerializer(data=request.data)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return Response(serializer.data, status=status.HTTP_201_CREATED)
+# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 #
-#     elif request.method == 'DELETE':
-#         recipe.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class RecipeDetail(APIView):
-#     """
-#     Retrieve, update or delete a snippet instance.
-#     """
-#     def get_object(self, pk):
-#         try:
-#             return Recipe.objects.get(pk=pk)
-#         except Recipe.DoesNotExist:
-#             raise Http404
+# # @api_view(['GET', 'PUT', 'DELETE'])
+# # def recipe_detail(request, pk, format=None):
+# #     """
+# #     Retrieve, update or delete a code snippet.
+# #     """
+# #     try:
+# #         recipe = Recipe.objects.get(pk=pk)
+# #     except Recipe.DoesNotExist:
+# #         return Response(status=status.HTTP_404_NOT_FOUND)
+# #
+# #     if request.method == 'GET':
+# #         serializer = RecipeSerializer(recipe)
+# #         return Response(serializer.data)
+# #
+# #     elif request.method == 'PUT':
+# #         data = JSONParser().parse(request)
+# #         serializer = RecipeSerializer(recipe, data=data)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return Response(serializer.data)
+# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# #
+# #     elif request.method == 'DELETE':
+# #         recipe.delete()
+# #         return Response(status=status.HTTP_204_NO_CONTENT)
 #
-#     def get(self, request, pk, format=None):
-#         recipe = self.get_object(pk)
-#         serializer = RecipeSerializer(recipe)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk, format=None):
-#         recipe = self.get_object(pk)
-#         serializer = RecipeSerializer(recipe, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk, format=None):
-#         recipe = self.get_object(pk)
-#         recipe.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+# # class RecipeDetail(APIView):
+# #     """
+# #     Retrieve, update or delete a snippet instance.
+# #     """
+# #     def get_object(self, pk):
+# #         try:
+# #             return Recipe.objects.get(pk=pk)
+# #         except Recipe.DoesNotExist:
+# #             raise Http404
+# #
+# #     def get(self, request, pk, format=None):
+# #         recipe = self.get_object(pk)
+# #         serializer = RecipeSerializer(recipe)
+# #         return Response(serializer.data)
+# #
+# #     def put(self, request, pk, format=None):
+# #         recipe = self.get_object(pk)
+# #         serializer = RecipeSerializer(recipe, data=request.data)
+# #         if serializer.is_valid():
+# #             serializer.save()
+# #             return Response(serializer.data)
+# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# #
+# #     def delete(self, request, pk, format=None):
+# #         recipe = self.get_object(pk)
+# #         recipe.delete()
+# #         return Response(status=status.HTTP_204_NO_CONTENT)
